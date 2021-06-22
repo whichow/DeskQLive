ffb4a79c0754f30d3dbf3850a9ac91becaf1d147
wuhao <wuhao@orbbec.com>
2021-06-22 19:17:31 +0800

    initial commit

---
+++ android/src/com/whichow/deskqlive/AndroidLauncher.java
@@ -0,0 +1,70 @@
+package com.whichow.deskqlive;
+
+import android.graphics.PixelFormat;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.whichow.deskqlive.QLive;
+
+public class AndroidLauncher extends AndroidApplication {
+    View qLiveView;
+
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        qLiveView = initializeForView(new QLive(), config);
+//		initialize(new QLive(), config);
+
+        final WindowManager windowManager = getWindowManager();
+        final WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            layoutParams.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+        } else {
+            layoutParams.type = WindowManager.LayoutParams.TYPE_PHONE;
+        }
+        layoutParams.width = 200;
+        layoutParams.height = 200;
+        layoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED;
+        layoutParams.format = PixelFormat.TRANSPARENT;
+
+        qLiveView.setOnTouchListener(new View.OnTouchListener() {
+            float lastX, lastY;
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                int x = (int)motionEvent.getRawX();
+                int y = (int)motionEvent.getRawY();
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        lastX = x;
+                        lastY = y;
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        layoutParams.x += (int) (x - lastX);
+                        layoutParams.y += (int) (y - lastY);
+                        windowManager.updateViewLayout(qLiveView, layoutParams);
+                        lastX = x;
+                        lastY = y;
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        break;
+                }
+                return true;
+            }
+        });
+
+        windowManager.addView(qLiveView, layoutParams);
+	}
+
+    @Override
+    protected void onDestroy() {
+	    getWindowManager().removeView(qLiveView);
+        super.onDestroy();
+    }
+}
