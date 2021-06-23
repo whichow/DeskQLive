d88328f8a3936711e7efc9ac5e0d8ca632e903f1
wuhao <wuhao@orbbec.com>
2021-06-23 19:30:38 +0800

    set gl clear color to transparent

--- core/src/com/whichow/deskqlive/QLive.java
+++ core/src/com/whichow/deskqlive/QLive.java
@@ -8,16 +8,16 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class QLive extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("dialog_device_waring.png");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0, 0, 0);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
